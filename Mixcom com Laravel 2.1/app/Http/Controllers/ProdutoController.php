00e432fb8c27e348b111ac32206ed267a5d0867d
layssbp <laysbrito17@gmail.com>
2018-10-15 21:41:57 -0300

    atualizaçao1

--- Mixcom com Laravel 2.1/app/Http/Controllers/ProdutoController.php
+++ Mixcom com Laravel 2.1/app/Http/Controllers/ProdutoController.php
@@ -1,11 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class Produtos extends Controller {
+class ProdutoController extends Controller {
 
 
 
