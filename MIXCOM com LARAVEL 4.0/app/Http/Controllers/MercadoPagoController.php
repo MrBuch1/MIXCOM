ed0cc47b10f3fa91a67714c9982439c75d1fe40c
Unknown <lucassantana.profissional@gmail.com>
2018-12-09 23:28:20 -0200

    MercadoPagoController 4.0

---
+++ MIXCOM com LARAVEL 4.0/app/Http/Controllers/MercadoPagoController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Exception;
+use MP;
+use App\Pedido;
+use App\Produto;
+use App\PedidoProduto;
+use App\Http\Controllers\CarrinhoController;
+use Illuminate\Support\Facades\DB;
+
+
+class MercadoPagoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        /*$pedido = PedidoProduto::where([
+            'pedido_id' => $idpedido,
+            'valor'    => $produto->valor
+        ])->get();*/
+
+        $pedido = DB::table('pedido_produtos')->select('valor');
+        
+        $preference_data = array (
+            "items" => array (
+                array (
+                    "title" => "Pagamento MIXCOM",
+                    "quantity" => 1,
+                    "currency_id" => "BRL",
+                    "unit_price" => $pedido
+                )
+            )
+        );
+
+        try {
+            $preference = MP::create_preference($preference_data);
+            return redirect()->to($preference['response']['init_point']);
+        } catch (Exception $e){
+            dd($e->getMessage());
+        }
+    }
+}
