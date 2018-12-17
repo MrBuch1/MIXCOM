ceced08e040cd7ed3450b3b661681b7eac432f28
Unknown <lucassantana.profissional@gmail.com>
2018-12-17 19:32:14 -0200

    Atualização no dropdown do navbar

--- MIXCOM com LARAVEL 4.0/resources/views/components/navbar.blade.php
+++ MIXCOM com LARAVEL 4.0/resources/views/components/navbar.blade.php
@@ -48,6 +48,12 @@
                     <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                         {{ __('Sair') }}
                     </a>
+                    <a class="dropdown-item" href="{{ route('carrinho.index') }}">
+                        {{ __('Carrinho') }}
+                    </a>
+                    <a class="dropdown-item" href="{{ route('carrinho.compras') }}">
+                        {{ __('Minhas Compras') }}
+                    </a>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                         @csrf
                     </form>
