10966190cd311390f181892c6448f60ad637dc91
layssbp <laysbrito17@gmail.com>
2019-01-11 15:59:37 -0300

    card de produtos

--- MIXCOM com LARAVEL 4.0/resources/views/produto/index.blade.php
+++ MIXCOM com LARAVEL 4.0/resources/views/produto/index.blade.php
@@ -20,7 +20,7 @@
                     @foreach($registros as $registro)
 					<div class="card table-responsive" style="width: 15em; word-wrap: break-word; margin: 5px">
 						<br>
-					<img src="{{ route('inicio') }}/storage/{{ $registro->imagem }}" class="img-responsive card-img-top" style= "align-self: center; width: 50%" >
+					<img src="{{ route('inicio') }}/storage/{{ $registro->imagem }}/100px180/" class="img-responsive card-img-top" style= "align-self: center; width: 50%" >
 						<div class="card-body">
 
 							<a href="{{ route('produto', $registro->id) }}">
