ffca5c7b8f66e1b7db54ee36d06a69ede72e5534
layssbp <laysbrito17@gmail.com>
2019-01-19 15:17:06 -0300

    listagem de usuários cadastrados

--- MIXCOM com LARAVEL 4.0/resources/views/admin/usuarios/index.blade.php
+++ MIXCOM com LARAVEL 4.0/resources/views/admin/usuarios/index.blade.php
@@ -1,35 +1,32 @@
 @extends('layouts.appAdmin', ["current"=>"usuarios"])
 
 @section('body')
-<h1>Pedidos</h1>
+<h1>Usuários</h1>
 
 <table class="table table-hover table-bordered table-striped">
     <thead>
         <tr>
             <th scope="col">Nome</th>
             <th scope="col">Email</th>
-            <th scope="col">Endereço</th>
             <th scope="col">Telefone</th>
-            <!-- <th scope="col">Status</th> -->
+            <th scope="col">Endereço</th>
             </tr>
         </thead>
         <tbody>
-        @foreach ($users as $u)
+        @foreach($users as $u)      
         <tr>
             <td>{{ $u->name }}</td>
-            <td>{{ $u->email }}
-                <!--@if ($p->id == $p->pedido_id)
-                {{ $p->name }}
-                @endif-->
-            </td>
-            <td>{{$u->telefone}}</td>
-            <!-- <td>{{$p->status}}</td> -->
-            <!--<td>
-                <a href="/produtos/editar/{{$p->id}}" class="btn btn-sm btn-primary">Editar</a>
-                <a href="/produtos/apagar/{{$p->id}}" class="btn btn-sm btn-danger">Apagar</a>
-            </td>-->
+            <td>{{ $u->email }}</td>
+            <td>{{ $u->telefone}}</td>
+        <td>
+            <b>Cidade: </b>{{ $u->cidade }}<br>    
+            <b>Estado: </b>{{ $u->uf }}<br>
+            <b>Rua: </b>{{ $u->cidade }} - <b>Número: </b>{{ $u->numero}} <br>
+            <b>Bairro: </b>{{ $u->bairro }} - <b>Cep: </b>{{ $u->cep }}<br>
+            <b>Complemento: </b>{{ $u->complemento }}
+        </td>
         </tr>
-        @endforeach
+        @endforeach    
     </tbody>
 </table>
 
